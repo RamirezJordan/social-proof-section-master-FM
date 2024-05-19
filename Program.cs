10abc8e6266d928d3e9c9542a765dfde1b956efa
Jordan Ramírez <ramjor377@gmail.com>
2024-05-19 08:27:39 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void myMethod(string fname)
+{
+    Console.WriteLine("Hola, " + fname);
+}
+
+static void Main(string[] args)
+{
+    myMethod("Jordan Ramírez");
+}
